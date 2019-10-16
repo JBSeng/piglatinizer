5296c9b19f1dfa92d2603b85671e5955216cc8d8
JBSeng <leaguepower84616@gmail.com>
2019-10-16 01:31:23 +0000

    October 15 PigLatinizer

--- js/script.js
+++ js/script.js
@@ -3,5 +3,14 @@ $("#button").click(function() {
     let message = $(".input").val();
     $(".output").text(message);
 });
+function checkVowel(str) {
+  var firstLetter=str[0];
+  var vowel=['a','e','i','o','u'];
+
+  if(vowel.includes(firstLetter)){
+    return 'starts with vowel';
+  }
+    return 'starts with consonant';
+  }
 
 });
